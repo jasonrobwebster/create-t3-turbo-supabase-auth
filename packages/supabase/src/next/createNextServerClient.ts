4d95c587d3be9c3da56d7cd19074935fde1f15f1
Jason <jason@strivemath.com>
2024-11-13 16:39:52 +0700

    install supabase & remove next-auth

---
+++ packages/supabase/src/next/createNextServerClient.ts
@@ -0,0 +1,35 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function createNextServerClient<Database>({
+  asAdmin = false,
+}: {
+  asAdmin?: boolean;
+} = {}) {
+  const cookieStore = cookies();
+
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    asAdmin
+      ? process.env.SUPABASE_SERVICE_KEY!
+      : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookies) {
+          try {
+            cookies.forEach(({ name, value, options }) => {
+              cookieStore.set({ name, value, ...options });
+            });
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
+}
