ec012939daee6a3415a8c9250b4055c0f536ff98
Jason <jason@strivemath.com>
2024-11-14 23:07:40 +0700

    insert supabase auth routes

---
+++ packages/auth/src/next/methods/confirm.ts
@@ -0,0 +1,35 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+import type { EmailOtpType } from "@acme/supabase";
+import { createNextServerClient } from "@acme/supabase/next";
+
+/**
+ * Confirms the user's email address or phone number.
+ */
+export async function confirm(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type") as EmailOtpType | null;
+  const next = searchParams.get("next") ?? "/";
+  const redirectTo = new URL(next);
+
+  if (token_hash && type) {
+    const supabase = createNextServerClient();
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    });
+
+    if (!error) {
+      return NextResponse.redirect(redirectTo);
+    }
+
+    console.error(error);
+  }
+
+  // return the user to an error page with some instructions
+  redirectTo.pathname = "/auth/auth-code-error";
+  return NextResponse.redirect(redirectTo);
+}
---
+++ packages/auth/src/next/methods/oauthConfirm.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import { createNextServerClient } from "@acme/supabase/next";
+
+export async function oauthConfirm(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next");
+  if (next) {
+    const nextUrl = new URL(next);
+    if (nextUrl.origin !== origin) {
+      return new Response("Invalid redirect URL", { status: 400 });
+    }
+  }
+  const redirectTo = new URL(next ?? origin);
+
+  if (code) {
+    const supabase = createNextServerClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(redirectTo);
+    }
+  }
+
+  // return the user to an error page with instructions
+  redirectTo.pathname = "/auth/auth-code-error";
+  return NextResponse.redirect(redirectTo);
+}
